822e2ba8ba464f2917e4405bd1b24d9f2756d7fc
David Ibia <david.ibia@boxmarshall.com>
2021-04-25 10:38:51 +0100

    feat: added padding to even out str display feat: passthrough str

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 const chalk = require('chalk')
-const { parseTime, stringify } = require('./utils')
+const { LogTypes } = require('./models')
+const { parseTime, collapse } = require('./utils')
 
 const format = (message) => {
   const { hours, minutes, seconds, milliseconds } = parseTime()
   const timestamp = chalk.gray.dim(
     `[${hours}:${minutes}:${seconds}:${milliseconds}]`
   )
-  return `${timestamp}${message}\n`
+  return `${timestamp} ${message}`
 }
 
-const display = (data) => {
-  const message = format(data)
-  process.stdout.write(message)
-  return message
+const display = (payload) => {
+  if (typeof payload == 'string') {
+    process.stdout.write(payload)
+    return payload
+  }
+
+  const { type, start, end, modifier = (x) => x, message = '' } = payload
+
+  const body = format(message)
+
+  let header = ''
+
+  const isBlank = type == LogTypes.blank
+
+  if (!isBlank) {
+    header = modifier(
+      `${type().toString()}`.padStart(start).padEnd(end).toLocaleUpperCase()
+    )
+  }
+
+  const data = `${header}${body}\n`
+
+  process.stdout.write(data)
+  return data
 }
 
 const operations = {
@@ -21,45 +42,85 @@ const operations = {
    * @param  {...any} data
    */
   log(...data) {
-    const message = stringify(data)
-    const colorCoded = `${chalk.gray(message)}`
-    return display(colorCoded)
+    const message = collapse(data)
+
+    const payload = {
+      type: LogTypes.log,
+      start: 6,
+      end: 10,
+      modifier: chalk.white.bgGray.bold,
+      message
+    }
+
+    return display(payload)
   },
   /**
    *
    * @param  {...any} data
    */
   warn(...data) {
-    const message = stringify(data)
-    const colorCoded = `${chalk.magenta(message)}`
-    return display(colorCoded)
+    const message = collapse(data)
+
+    const payload = {
+      type: LogTypes.warn,
+      start: 7,
+      end: 10,
+      modifier: chalk.white.bgHex('#ffa500').bold,
+      message
+    }
+
+    return display(payload)
   },
   /**
    *
    * @param  {...any} data
    */
   info(...data) {
-    const message = stringify(data)
-    const colorCoded = `${chalk.blue(message)}`
-    return display(colorCoded)
+    const message = collapse(data)
+
+    const payload = {
+      type: LogTypes.info,
+      start: 7,
+      end: 10,
+      modifier: chalk.white.bgBlue.bold,
+      message
+    }
+
+    return display(payload)
   },
   /**
    *
    * @param  {...any} data
    */
   error(...data) {
-    const message = stringify(data)
-    const colorCoded = `${chalk.red(message)}`
-    return display(colorCoded)
+    const message = collapse(data)
+
+    const payload = {
+      type: LogTypes.error,
+      start: 8,
+      end: 10,
+      modifier: chalk.white.bgRed.bold,
+      message
+    }
+
+    return display(payload)
   },
   /**
    *
    * @param  {...any} data
    */
   blank(...data) {
-    const message = stringify(data)
-    const colorCoded = `${chalk.gray.dim(message)}`
-    return display(colorCoded)
+    const message = collapse(data)
+
+    const payload = {
+      type: LogTypes.blank,
+      start: 8,
+      end: 10,
+      modifier: chalk.white.bgGray.dim,
+      message
+    }
+
+    return display(payload)
   }
 }
 
