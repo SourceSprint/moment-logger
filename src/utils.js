47a9e54b1d9d9514ddd302a4bfc3f9a71cc95191
David Ibia <david.ibia@boxmarshall.com>
2021-07-23 14:21:30 +0100

    feat: added throttle queue

--- src/utils.js
+++ src/utils.js
@@ -2,10 +2,20 @@ const replacer = (key, value) => {
   return value
 }
 
+/**
+ * Parse object into readable format
+ * @param {Object} data
+ * @returns {String}
+ */
 const handleObject = (data) => {
   return `${typeof data}\n${JSON.stringify(data, replacer, '\t')}`
 }
 
+/**
+ * Parse error into readable format
+ * @param {Error} error
+ * @returns {String}
+ */
 const handleError = (error) => {
   const { name = '', message = '' } = error
   return handleObject({ name, message })
@@ -30,6 +40,14 @@ const collapse = (data) => {
   return composed.join('\n')
 }
 
+/**
+ * Get current time
+ * @returns {Object} time
+ * @returns {int} time.hours Current hour
+ * @returns {int} time.minutes Current minute
+ * @returns {int} time.seconds Current second
+ * @returns {int} time.milliseconds Current milliseconds
+ */
 const parseTime = () => {
   const date = new Date()
   const hours = date.getHours()
@@ -40,7 +58,53 @@ const parseTime = () => {
   return { hours, minutes, seconds, milliseconds }
 }
 
+// Throttle queue implementation https://www.collegestash.com/throttling-using-queue/
+
+/**
+ * Throttle Queue
+ * @param {function} fn Function call
+ * @param {Number} delay Throttle delay
+ * @returns {function}
+ */
+const throttle = (fn, delay) => {
+  if (isNaN(delay)) {
+    throw new Error('Delay must be a number')
+  }
+
+  let timeout = null
+  let nodelay = true
+  let queue = []
+
+  const start = () => {
+    if (queue.length) {
+      const first = queue.shift()
+      fn.apply(first.context, first.arguments)
+      timeout = setTimeout(start, delay)
+    } else {
+      nodelay = true
+    }
+  }
+
+  const ret = (...rest) => {
+    queue.push({
+      context: this,
+      arguments: [...rest]
+    })
+    if (nodelay) {
+      nodelay = false
+      start()
+    }
+  }
+
+  ret.reset = () => {
+    clearTimeout(timeout)
+    queue = []
+  }
+  return ret
+}
+
 module.exports = {
-  parseTime,
-  collapse
+  collapse,
+  throttle,
+  parseTime
 }
